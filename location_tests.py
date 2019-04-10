ee8cc886b76e884aead0674a8ce55cdb0617f4fe
Wesley Reynolds <wes.reynolds505@gmail.com>
2019-04-10 14:37:20 -0700

    I made these changes

--- location_tests.py
+++ location_tests.py
@@ -1,13 +1,39 @@
+# Wesley Reynolds
+# Paul Hatalsky
+# CPE 202
+# CPE 202 Lab 0
+
 import unittest
 from location import *
 
 class TestLab1(unittest.TestCase):
-
     def test_repr(self):
-        loc = Location("SLO", 35.3, -120.7)
-        self.assertEqual(repr(loc),"Location('SLO', 35.3, -120.7)")
+        # Locations
+        loc1 = Location("SLO", 35.3, -120.7)
+        loc2 = Location("Paris", 48.9, 2.4)
+        loc3 = Location("SLO", 35.3, -120.7)
+        loc4 = loc1
+
+        # Test cases
+        self.assertEqual(repr(loc1),"Location('SLO', 35.3, -120.7)")
+        self.assertEqual(repr(loc2),"Location('Paris', 48.9, 2.4)")
+        self.assertEqual(repr(loc3),"Location('SLO', 35.3, -120.7)")
+        self.assertEqual(repr(loc4),"Location('SLO', 35.3, -120.7)")
     
-    # Add more tests!
+    def test_eq(self):
+        # Locations
+        loc1 = Location("SLO", 35.3, -120.7)
+        loc2 = Location("Paris", 48.9, 2.4)
+        loc3 = Location("SLO", 35.3, -120.7)
+        loc4 = loc1
+
+        # Test cases
+        self.assertFalse(loc1 == loc2)
+        self.assertTrue(loc1 == loc3)
+        self.assertTrue(loc1 == loc4)
+        self.assertFalse(loc2 == loc3)
+        self.assertFalse(loc2 == loc4)
+        self.assertTrue(loc3 == loc4)
 
 if __name__ == "__main__":
         unittest.main()
